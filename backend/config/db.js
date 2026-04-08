32e3e11d3bcf760ae1b678a60ddc3e432e829777
JasonAMBA <ambajason2@gmail.com>
2026-04-08 17:39:25 +0200

    Fix: passage à mysql.createPool pour éviter les crashes de connexion

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,19 +2,14 @@ require('dotenv').config();
 const mysql = require('mysql2');
 
 // Configuration MySQL
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Erreur lors de la connexion à MySQL', err);
-    return;
-  }
-  console.log('Connecté à la base de donnée MySQL');
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
